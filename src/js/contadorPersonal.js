0d3d4102849a354c0156f0d208bfdcb2dca40bd7
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-14 00:18:36 +0200

    Tamanho fuentes

--- src/js/contadorPersonal.js
+++ src/js/contadorPersonal.js
@@ -5,10 +5,10 @@ export function contadorPersonal(juego = 501, nombre = "Guest") {
 
         <div class="flex flex-col justify-center align-center bg-red-700 border border-black"> 
         <div class="flex justify-center align-top p-2 py-4 border-b border-black">
-            <p class="flex justify-center align-center text-4xl">${juego}</p>
+            <p class="flex justify-center align-center text-4xl font-semibold">${juego}</p>
         </div>
         <div class="flex justify-center align-center border-t border-black">
-            <p class="flex justify-center align-center">${nombre}</p>
+            <p class="flex justify-center align-center text-xl">${nombre}</p>
         </div>
         </div>
         
@@ -16,26 +16,26 @@ export function contadorPersonal(juego = 501, nombre = "Guest") {
         <div class="bg-gray-200 flex flex-col justify-center align-center border border-black">
         <div class="flex justify-center align-center bg-blue-600 border-b border-black">
             <div class="flex justify-center align-center p-3 border-r border-black">
-            <p>0</p>
+            <p class="text-xl">0</p>
             </div>
             <div class="flex justify-center align-center p-3 border-x border-black">
-            <p>1</p>
+            <p class="text-xl">1</p>
             </div>
             <div class="flex justify-center align-center p-3 border-l border-black">
-            <p>2</p>
+            <p class="text-xl">2</p>
             </div>
         </div>
 
         <div class="flex justify-center align-center p-3 bg-blue-600 border-y border-black">
-            <p>total</p>
+            <p class="text-xl">total</p>
         </div>
         </div>
 
 
         <div class="bg-yellow-500 flex flex-col justify-center items-center border border-l-2 border-black">
         <div class="flex justify-center align-center flex-col">
-            <p class="flex justify-center items-center">MEDIA</p>
-            <p class="flex justify-center items-center">50</p>
+            <p class="flex justify-center items-center text-xl">MEDIA</p>
+            <p class="flex justify-center items-center text-2xl">50</p>
         </div>
         </div>
     </div>
