3b3beed88f96cee7d8238c72e96be595538c081a
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-26 21:26:36 +0200

    Redisenho estilos

--- src/js/contadorPersonal.js
+++ src/js/contadorPersonal.js
@@ -1,10 +1,10 @@
 /* Funcion que recibe la cantidad e puntos para empezar la partida y el nombre del jugador y devuelve el html con los datos personalizados*/
-export function contadorPersonal(game = 501, nombre = "Guest", id) {
+export function contadorPersonal(game = 501, nombre = "Guest", id, color) {
 
     return `
         <div class="grid grid-cols-3 mt-4 border-2 border-black max-w-xm mx-2">
 
-        <div class="flex flex-col justify-center align-center bg-red-700 border border-black"> 
+        <div class="flex flex-col justify-center align-center bg-${color} border border-black"> 
         <div class="flex justify-center align-top p-2 py-4 border-b border-black">
             <p id="counterP${id}" class="flex justify-center align-center text-4xl font-semibold">${game}</p>
         </div>
@@ -15,7 +15,7 @@ export function contadorPersonal(game = 501, nombre = "Guest", id) {
         
         
         <div class="bg-gray-200 flex flex-col justify-center align-center border border-black">
-        <div class="flex justify-center align-center bg-blue-600 border-b border-black">
+        <div class="flex justify-center align-center bg-${color} border-b border-black">
             <div class="flex justify-center align-center px-2 py-3 min-w-[39px] border-r border-black">
             <p id="dart1-P${id}" class="text-xl">--</p>
             </div>
@@ -27,13 +27,13 @@ export function contadorPersonal(game = 501, nombre = "Guest", id) {
             </div>
         </div>
 
-        <div class="flex justify-center align-center p-3 bg-blue-600 border-t border-black">
+        <div class="flex justify-center align-center p-3 bg-${color} border-t border-black">
             <p id="partialAverageP${id}" class="text-xl">-</p>
         </div>
         </div>
 
 
-        <div class="bg-yellow-500 flex flex-col justify-center items-center border border-l-2 border-black">
+        <div class="bg-${color} flex flex-col justify-center items-center border border-l-2 border-black">
         <div class="flex justify-center align-center flex-col">
             <p id="mediaTextP${id}" class="flex justify-center items-center text-xl">MEDIA</p>
             <p id="totalAverageP${id}" class="flex justify-center items-center text-2xl">-</p>
--- src/js/showGameStats.js
+++ src/js/showGameStats.js
@@ -5,8 +5,8 @@ export function show(){
     const player1Counter = document.getElementById("player1");
     const player2Counter = document.getElementById("player2");
     
-    player1Counter.innerHTML = contadorPersonal(localStorage.getItem('points'), localStorage.getItem('player1'), 1);
-    player2Counter.innerHTML = contadorPersonal(localStorage.getItem('points'), localStorage.getItem('player2'), 2);
+    player1Counter.innerHTML = contadorPersonal(localStorage.getItem('points'), localStorage.getItem('player1'), 1, "blue-600");
+    player2Counter.innerHTML = contadorPersonal(localStorage.getItem('points'), localStorage.getItem('player2'), 2, "red-600");
     
     //Modificar el tipo de media segun lo que se seleccione en el formulario
     const mediaTextP1 = document.getElementById("mediaTextP1");
